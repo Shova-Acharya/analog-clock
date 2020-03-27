5839fb3fa4d780c11ce883bce7d1491e9e2d2fad
Shova-Acharya <shova.723@gmail.com>
2020-03-27 12:18:44 +0545

    design changes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+
 const currentSec = document.querySelector(".secline");
 const currentMin = document.querySelector(".minline");
 const currentHour = document.querySelector(".hourline");
 
+time();
+
 function time(){
     const date = new Date();
-    const hour = date.getHours() * 30; //360/12
+    const hour = (date.getHours() * 30); //360/12
     const min = date.getMinutes() * 6; //360/60
     const sec = date.getSeconds() * 6; //360/60
 
@@ -19,5 +22,5 @@ function time(){
     setTimeout(time,1000);   
 }
 
-time();
+
 
